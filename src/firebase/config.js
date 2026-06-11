b2e8a9e28021d942448efe16a70259727ba5def3
Rashani Adhithya <rashaniadhithya@gmail.com>
2026-06-11 11:30:06 +0530

    Initial project setup

---
+++ src/firebase/config.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC_YtpyyLR-1FBrQf4cfO9C7j4vGpf3p88",
+  authDomain: "mrt-asset-management-system.firebaseapp.com",
+  projectId: "mrt-asset-management-system",
+  storageBucket: "mrt-asset-management-system.firebasestorage.app",
+  messagingSenderId: "456700819221",
+  appId: "1:456700819221:web:4eec7624f53deaf3199b1c"
+};
+
+const app = initializeApp(firebaseConfig);
+
+
+export default app;
